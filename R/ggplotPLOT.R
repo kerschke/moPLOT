e1c9fbf21dcd61f10b7a3d90b9e0b2903a5984ec
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2020-07-11 17:33:28 +0200

    Some data checks

--- R/ggplotPLOT.R
+++ R/ggplotPLOT.R
@@ -1,15 +1,25 @@
 #' @export
-ggplotPLOT = function(dec.space, obj.space, sinks, height) {
+ggplotPLOT = function(dec.space, obj.space, sinks, height, check.data = TRUE) {
+  if (check.data) {
+    assertMatrix(dec.space, ncols = 2, col.names = "unique") # 2D only
+    assertMatrix(obj.space, min.cols = 2, nrows = nrow(dec.space)) # at least 2-objective
+    
+    assertInteger(sinks, lower = 1, upper = nrow(dec.space)) # sinks are valid indices for observations
+    assertNumeric(height, len = nrow(dec.space)) # one height value per observation
+  }
+  
+  # Compute coloring of locally efficient points
   
   nds = ecr::doNondominatedSorting(t(obj.space[sinks,]))
+  
+  # Impute Gradient Field Heatmap height
   
   height[height == 0] = min(height[height > 0]) / 2
   
   height.df = cbind.data.frame(dec.space, height=height)
-  height.df = height.df[order(height.df$height, decreasing=T),]
   
   sinks.df = cbind.data.frame(dec.space[sinks,], rank=nds$dom.counter+1)
-  sinks.df = sinks.df[order(sinks.df$rank, decreasing=T),]
+  sinks.df = sinks.df[order(sinks.df$rank, decreasing=T),] # Ensure more efficient points are plotted later, i.e. on top
   
   g = ggplotHeatmap(height.df, color.palette = gray.colors(500L, start = 0, end = 1, gamma = 0.5)) +
     geom_point(mapping = aes(x1, x2, color=rank), data = sinks.df) +
