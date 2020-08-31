673f623713817d60928797d531a58b0f65c07807
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2020-08-31 23:44:04 +0200

    Add Aspar functions

--- inst/dashboard/dashboardFunctions.R
+++ inst/dashboard/dashboardFunctions.R
@@ -73,29 +73,35 @@ f3_1 = function(x) ((x[1] + 0.25) ** 4 + 3 * (x[2] - 1) ** 2)
 f_2d2d = function(x) c(f1_1(x), f2_1(x))
 f_2d3d = function(x) c(f1_1(x), f2_1(x), f3_1(x))
 
-test.2d.2d = smoof::makeMultiObjectiveFunction(name = "2D->2D Test Function", id = "test_2d2d", description = "", fn = f_2d2d,
-                                               par.set = ParamHelpers::makeNumericParamSet(len = 2, lower = c(-2,-1), upper = c(2,3)))
-
-test.2d.3d = smoof::makeMultiObjectiveFunction(name = "2D->3D Test Function", id = "test_2d3d", description = "", fn = f_2d3d,
-                                               par.set = ParamHelpers::makeNumericParamSet(len = 2, lower = c(-2,-1), upper = c(2,3)))
-
 f1_2 = function(x) (x[1]**4 - 2*x[1]**2 + x[2]**2 + 1 + x[3] ** 2)
 f2_2 = function(x) ((x[1] + 0.5)**2 + (x[2]-2)**2 + (x[3] - 1) ** 4)
 f3_2 = function(x) ((x[1] + 0.25) ** 4 + 3 * (x[2] - 1) ** 2 + (x[3] - 1) ** 2)
 f_3d2d = function(x) c(f1_2(x), f2_2(x))
 f_3d3d = function(x) c(f1_2(x), f2_2(x), f3_2(x))
 
-test.3d.2d = smoof::makeMultiObjectiveFunction(name = "3D->2D Test Function", id = "test_3d2d", description = "", fn = f_3d2d,
-                                               par.set = ParamHelpers::makeNumericParamSet(len = 3, lower = c(-2,-1,-2), upper = c(2,3,2)))
-
-test.3d.3d = smoof::makeMultiObjectiveFunction(name = "3D->3D Test Function", id = "test_3d3d", description = "", fn = f_3d3d,
-                                               par.set = ParamHelpers::makeNumericParamSet(len = 3, lower = c(-2,-1,-2), upper = c(2,3,2)))
+makeAsparFunction <- function(dimensions, n.objectives) {
+  if (dimensions == 2 && n.objectives == 2) {
+    smoof::makeMultiObjectiveFunction(name = "2D->2D Test Function", id = "test_2d2d", description = "", fn = f_2d2d,
+                                      par.set = ParamHelpers::makeNumericParamSet(len = 2, lower = c(-2,-1), upper = c(2,3)))
+  } else if (dimensions == 2 && n.objectives == 3) {
+    smoof::makeMultiObjectiveFunction(name = "2D->3D Test Function", id = "test_2d3d", description = "", fn = f_2d3d,
+                                      par.set = ParamHelpers::makeNumericParamSet(len = 2, lower = c(-2,-1), upper = c(2,3)))
+  } else if (dimensions == 3 && n.objectives == 2) {
+    smoof::makeMultiObjectiveFunction(name = "3D->2D Test Function", id = "test_3d2d", description = "", fn = f_3d2d,
+                                      par.set = ParamHelpers::makeNumericParamSet(len = 3, lower = c(-2,-1,-2), upper = c(2,3,2)))
+  } else if (dimensions == 3 && n.objectives == 3) {
+    smoof::makeMultiObjectiveFunction(name = "3D->3D Test Function", id = "test_3d3d", description = "", fn = f_3d3d,
+                                      par.set = ParamHelpers::makeNumericParamSet(len = 3, lower = c(-2,-1,-2), upper = c(2,3,2)))
+  }
+}
 
 other_functions = list(
-  "Viennet Function" = smoof::makeViennetFunction,
-  "Kursawe" = smoof::makeKursaweFunction # TODO Dimensions
-  # "2D->2D Test Function" = test.2d.2d, # TODO convert to generator function
-  # "2D->3D Test Function" = test.2d.3d,
-  # "3D->2D Test Function" = test.3d.2d,
-  # "3D->3D Test Function" = test.3d.3d
+  "Aspar" = makeAsparFunction,
+  # "BiSphere" = smoof::makeBiSphereFunction, # does not work as expected and is kinda boring
+  "BK1" = smoof::makeBK1Function,
+  "Dent" = smoof::makeDentFunction,
+  "ED1" = smoof::makeED1Function,
+  "ED2" = smoof::makeED2Function,
+  "Kursawe" = smoof::makeKursaweFunction,
+  "Viennet" = smoof::makeViennetFunction
 )
