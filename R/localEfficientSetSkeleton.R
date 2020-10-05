5183306ad488b02b6b1f9362c33fb410b3f5299d
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2020-10-05 22:35:14 +0200

    Cleanup

--- R/localEfficientSetSkeleton.R
+++ R/localEfficientSetSkeleton.R
@@ -1,28 +1,21 @@
 #' @export
 localEfficientSetSkeleton = function(design, gradients, divergence, integration = "fast", with.basins = FALSE) {
   less = list()
-
+  
   cat('Finding critical points ...\n')
-
-  # lnd = lapply(1:ncol(grid$obj.space), function(i) {
-  #   locallyNondominatedCPP(matrix(grid$obj.space[,i]), grid$dims, T)
-  # })
   
   lnd = locallyNondominatedCPP(design$obj.space, design$dims, TRUE)
-
   critical = getCriticalPointsCellCPP(gradients$multi.objective, gradients$single.objective, divergence, lnd, design$dims, sinks_only = TRUE)
 
   sinks = critical$sinks
-
-  # less$so.min = list()
-  # less$so.min[[1]] = intersect(critical.1$sinks, sinks)
-  # less$so.min[[2]] = intersect(critical.2$sinks, sinks)
 
   cat('Integrating vector field ...\n')
   if (integration == "fast") {
     integrated = computeCumulatedPathLengths(design$dec.space, gradients$multi.objective, sinks, fix.diagonals = T, prec.vector.length = 0, prec.norm = 0)
   } else {
     integrated = integrateVectorField(gradients$multi.objective, design$dims, sinks)
+    dim(integrated$height) = c(length(integrated$height), 1)
+    colnames(integrated$height) = c("height")
   }
 
   less$height = integrated$height
@@ -32,7 +25,7 @@ localEfficientSetSkeleton = function(design, gradients, divergence, integration 
   if (with.basins) {
     integration.sinks = sort(unique(integrated$last.visited))
 
-    ccs = connectedComponentsGrid(integration.sinks, grid$dims)
+    ccs = connectedComponentsGrid(integration.sinks, design$dims)
     valid.ccs.ids = (ccs != 0 & !(ccs %in% as.numeric(names(table(ccs)))[table(ccs) < 4]))
     valid.ccs = ccs[valid.ccs.ids]
     valid.sinks = integration.sinks[valid.ccs.ids]
@@ -40,7 +33,7 @@ localEfficientSetSkeleton = function(design, gradients, divergence, integration 
     less$sinks = valid.sinks
 
     cat('3/3 Calculating basins of attraction ...\n')
-    sink.to.basin = rep(-1, prod(grid$dims))
+    sink.to.basin = rep(-1, prod(design$dims))
     sink.to.basin[valid.sinks] = valid.ccs
 
     basins = sapply(1:length(integrated$last.visited), function(i) {
@@ -49,7 +42,7 @@ localEfficientSetSkeleton = function(design, gradients, divergence, integration 
 
     less$basins = basins
 
-    ridges = changeOfBasin(basins, grid$dims)
+    ridges = changeOfBasin(basins, design$dims)
     ridges = union(ridges, which(basins == -1))
 
     less$ridges = ridges
