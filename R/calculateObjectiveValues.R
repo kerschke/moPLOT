bd532f2a43f3b33799948b3c9e4779dbbdcbf49f
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2021-08-14 23:41:08 +0200

    Allow vectorized computation (if available)

--- R/calculateObjectiveValues.R
+++ R/calculateObjectiveValues.R
@@ -1,11 +1,15 @@
 #' @export
-calculateObjectiveValues = function(points, fn, parallelize = FALSE, parallel.cores = (parallel::detectCores() - 1)) {
+calculateObjectiveValues = function(points, fn, parallelize = FALSE, parallel.cores = (parallel::detectCores() - 1),
+                                    vectorized.evaluation = TRUE) {
   n = smoof::getNumberOfObjectives(fn)
   names = paste0("y", 1:n)
   
   cat("Evaluating grid of objective values ...\n")
   
-  if (parallelize) {
+  if (vectorized.evaluation && smoof::isVectorized(fn)) {
+    # Experimental feature!
+    obj.space = fn(t(points))
+  } else if (parallelize) {
     cuts = cut(1:nrow(points), parallel.cores)
     r = parallel::mclapply(levels(cuts), function(i) {
       t(apply(points[cuts == i,], 1, fn))
