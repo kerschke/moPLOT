9d029ac32fa8a2bf1b1b28f862460ac74c4eda45
Lennart Schäpermeier <lennart.schaepermeier@tu-dresden.de>
2022-02-24 14:49:20 +0100

    Layout modifications to Local PCP visualization
    - reduce default alphaLines
    - sort plotted points by basin
    - use viridis color scale
    - use theme_minimal()
    - alpha = 1 in legend

--- R/ggplotPCP.R
+++ R/ggplotPCP.R
@@ -12,18 +12,19 @@
 #'
 #' @export
 ggplotLocalPCP <- function(obj.space, basins, scale = "uniminmax",
-                           alphaLines = 0.2, check.data = TRUE) {
+                           alphaLines = 0.1, check.data = TRUE) {
   if (check.data) {
     assertIntegerish(basins)
     assertNumeric(obj.space)
     assertNumeric(alphaLines, lower = 0, upper = 1)
-    
+
     assertMatrix(obj.space, min.cols = 2L, nrows = length(basins))
   }
 
   objectives <- ncol(obj.space)
-  
+
   combine_df <- cbind.data.frame(obj.space, basin = as.factor(basins))
+  combine_df <- combine_df[order(combine_df$basin), ]
 
   ggparcoord(
     combine_df,
@@ -43,7 +44,9 @@ ggplotLocalPCP <- function(obj.space, basins, scale = "uniminmax",
         "y3" = expression(y[3])
       ),
       expand = expansion(mult = 0.1)
-    )
+    ) + scale_color_viridis_d() +
+    guides(colour = guide_legend(override.aes = list(alpha = 1))) +
+    theme_minimal()
 }
 
 #' Global Multi-Objective PCP Visualizations
@@ -64,7 +67,7 @@ ggplotGlobalPCP <- function(obj.space, scale = "uniminmax",
     assertNumeric(obj.space)
     assertNumeric(alphaLines, lower = 0, upper = 1)
   }
-  
+
   objectives <- ncol(obj.space)
 
   ggparcoord(
@@ -83,5 +86,6 @@ ggplotGlobalPCP <- function(obj.space, scale = "uniminmax",
         "y3" = expression(y[3])
       ),
       expand = expansion(mult = 0.1)
-    )
+    ) +
+    theme_minimal()
 }
